19b74f83100d4d01fee03dae1dc26a96ae11b792
LiangYT <381518188@qq.com>
2019-04-04 17:36:19 +0800

    初始化！

---
+++ app/src/main/java/com/sinothk/demo/android/res/AndroidResDemoMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.demo.android.res;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidResDemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_res_demo_main);
+    }
+}
